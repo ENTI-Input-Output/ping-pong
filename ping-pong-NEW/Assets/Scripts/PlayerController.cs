8ea36fabb5acd58efaa30d81738b6662a3cc1fea
AlexRivero <alexriveroferras@enti.cat>
2020-03-26 17:08:30 +0100

    Reestructured repo
    Created new project from scratch

---
+++ ping-pong-NEW/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Hand LeftHand;
+    public GameObject Ball;
+
+    private void Update()
+    {
+        //Trigger Pull
+        if (SteamVR_Actions.default_GenerateBall.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            if (Ball.GetComponent<Interactable>().attachedToHand == null)
+            {
+                LeftHand.AttachObject(Ball, GrabTypes.Trigger);
+                LeftHand.HoverLock(Ball.GetComponent<Interactable>());
+            }
+        }
+
+        //Trigger Release
+        if (SteamVR_Actions.default_GenerateBall.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            LeftHand.DetachObject(Ball);
+            LeftHand.HoverUnlock(Ball.GetComponent<Interactable>());
+        }
+    }
+}
