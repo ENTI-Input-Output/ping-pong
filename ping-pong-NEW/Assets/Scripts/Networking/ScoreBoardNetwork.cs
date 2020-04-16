0752486599f7fbcd9df59deef883b72cf848cdee
Max Boet Testa <max_wapo97@hotmail.com>
2020-04-16 17:17:13 +0200

    scoreboard working

---
+++ ping-pong-NEW/Assets/Scripts/Networking/ScoreBoardNetwork.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ScoreBoardNetwork : MonoBehaviourPun
+{
+    TextMeshProUGUI scoreText;
+    public int P1 = 0;
+    public int P2 = 0;
+    private PhotonView PV;
+
+    [PunRPC]
+    void UpdateScorePointP1()
+    {
+        P1++;
+        UpdateScoreLocal();
+    }
+
+    [PunRPC]
+    void UpdateScorePointP2()
+    {
+        P2++;
+        UpdateScoreLocal();
+    }
+
+    void UpdateScoreLocal()
+    {
+        scoreText.text = P1.ToString() + ":" + P2.ToString();
+    }
+
+    void Start()
+    {
+        PV = transform.GetComponent<PhotonView>();
+        scoreText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PV.RPC("UpdateScorePointP1", RpcTarget.AllBuffered);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PV.RPC("UpdateScorePointP2", RpcTarget.AllBuffered);
+        }
+    }
+  
+
+
+    /*
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+        }
+
+        else
+        {
+        }
+    }*/
+}
