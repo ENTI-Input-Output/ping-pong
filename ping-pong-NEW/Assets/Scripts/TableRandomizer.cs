857c381817bc283e99fcc6f5314090408a96067a
David Recuenco <thedavidro@gmail.com>
2020-05-07 19:26:13 +0200

    Added table randomizer system
    Test scene + placeholder table prefabs

---
+++ ping-pong-NEW/Assets/Scripts/TableRandomizer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableRandomizer : MonoBehaviour {
+    public GameObject[] tables;
+
+    private int currentTable;
+    private int oldTable;
+
+    void Start() { // Default table as first
+        currentTable = 0;
+        oldTable = 0;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) ChangeTable(); // Input for testing
+    }
+
+    public void ChangeTable() {
+        System.Random rand = new System.Random();
+        int nextTable;
+        do {
+            nextTable = rand.Next(tables.Length);
+        } while (nextTable == oldTable);
+
+        oldTable = nextTable;
+        UpdateTables(nextTable);
+        Debug.Log(nextTable);
+    }
+
+    private void UpdateTables(int chosenTable) {
+        for (int i = 0; i < tables.Length; i++) {
+            if (i == chosenTable) tables[i].SetActive(true);
+            else tables[i].SetActive(false);
+        }
+    }
+}
