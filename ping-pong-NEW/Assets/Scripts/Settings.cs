8178b270a9b8676a924170521d0d07582f99fb4b
David <recuero.david@gmail.com>
2020-05-14 17:30:11 +0200

    Main Menu

---
+++ ping-pong-NEW/Assets/Scripts/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    public void MusicSlider()
+    {
+        //manage
+    }
+
+    public void FXSlider()
+    {
+        //manage
+    }
+    public void BackButton()
+    {
+        MainMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
