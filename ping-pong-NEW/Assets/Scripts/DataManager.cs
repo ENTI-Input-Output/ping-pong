ab40ece865b1cc0e0a3be07579948f9227a14e4f
AlexRivero <alexriveroferras@enti.cat>
2020-05-21 01:54:55 +0200

    Added main menu interaction using VR controllers

---
+++ ping-pong-NEW/Assets/Scripts/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    #region Singleton
+    public static DataManager Instance;
+
+    //Awake
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public bool IsPlayer = false;
+
+}
--- ping-pong-NEW/Assets/Scripts/UI/MainMenu.cs
+++ ping-pong-NEW/Assets/Scripts/UI/MainMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 using Photon.Pun;
@@ -7,29 +8,59 @@ using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Main;
     public GameObject Settings;
     public GameObject RegularMode;
     public GameObject TargetMode;
-    
+    public GameObject Player;
+
+    [SerializeField]
+    private string SceneToLoad;
+
     public void RegularModeMatchButton()
     {
         RegularMode.SetActive(true);
-        this.gameObject.SetActive(false);
+        Main.SetActive(false);
+
+        SceneToLoad = "PlayScene";
     }
 
     public void TargetModeMatchButton()
     {
         TargetMode.SetActive(true);
-        this.gameObject.SetActive(false);
+        Main.SetActive(false);
     }
+
     public void SettingsButton()
     {
         Settings.SetActive(true);
-        this.gameObject.SetActive(false);
+        Main.SetActive(false);
     }
+
     public void ExitButton()
     {
         Application.Quit();
     }
-    
+
+    public void OnPlayClick()
+    {
+        DataManager.Instance.IsPlayer = true;
+        Destroy(Player);
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void OnObserverClick()
+    {
+        DataManager.Instance.IsPlayer = false;
+        Destroy(Player);
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void OnBackClick()
+    {
+        Main.SetActive(true);
+        RegularMode.SetActive(false);
+        TargetMode.SetActive(false);
+        Settings.SetActive(false);
+    }
 }
